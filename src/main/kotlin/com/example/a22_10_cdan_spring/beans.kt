c950157266d8d0169567df17d1cc173ffd5b03b2
AnthonyPCPortable <anth06ny@gmail.com>
2023-04-17 08:58:42 +0200

    TP AIRPORT

--- src/main/kotlin/com/example/a22_10_cdan_spring/beans.kt
+++ src/main/kotlin/com/example/a22_10_cdan_spring/beans.kt
@@ -2,4 +2,4 @@ package com.example.a22_10_cdan_spring
 
 data class StudentBean(var name: String = "", var note: Int=0)
 
-data class PlaneBean(var name: String, var id:String)
+data class PlaneBean(var name: String?, var id:String?)
